22eeaaed002022d60baa2c67e367791fb6976173
vvedition <alpaz@knoah.com>
2020-09-20 19:36:57 -0600

    Commit segundo cambio

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Cuando podas ver esto, significa que ya podemos empezar con el proyecto");
-        //Probando commits, pussh y pull
+        //Probando commits, push y pull
 
-
+        //segunda linea de prueba de commits
     }
 }
